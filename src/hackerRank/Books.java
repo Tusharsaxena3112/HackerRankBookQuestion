0f2e20942d9bbe7da1c955ac95e496975c41a018
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-17 21:58:37 +0530

    adding constructor for Books.java

---
+++ src/hackerRank/Books.java
@@ -0,0 +1,13 @@
+package hackerRank;
+
+public class Books {
+    private String name;
+    private String author;
+    private String number;
+
+    public Books(String name, String author, String number) {
+        this.name = name;
+        this.author = author;
+        this.number = number;
+    }
+}
