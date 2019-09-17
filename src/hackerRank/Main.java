48d1cfdf83074ab5b3dad1bd7e30b9eaec65bb41
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-17 22:03:23 +0530

    completing Main.java

--- src/hackerRank/Main.java
+++ src/hackerRank/Main.java
@@ -9,6 +9,20 @@ public class Main {
         if (n != 0) {
             input.nextLine().trim();
         }
-
+        Books a[] = new Books[n];
+        for (int i = 0; i <= n - 1; i++) {
+            String name = input.nextLine();
+            String author = input.nextLine();
+            String number = input.nextLine();
+            a[i] = new Books(name, author, number);
+        }
+        if (a.length != 0) {
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i]);
+            }
+        } else {
+            System.out.println("N/A");
+        }
     }
+
 }
