18445da6b1573d8ebd719db0b1d64de4e7a2cbe2
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-25 23:56:31 +0530

    adding try catch block for NegativeArraySize

--- src/exceptionHandling/ExceptionHandling.java
+++ src/exceptionHandling/ExceptionHandling.java
@@ -1,15 +1,18 @@
 package exceptionHandling;
 
-import java.util.Random;
-
 public class ExceptionHandling {
     public static void main(String[] args) {
         try {
-            Random input = new Random();
+            // Random input = new Random()
+            // int random=input.nextInt();
+            int[] array = new int[-5];//Here it can generate exception .
+            System.out.println("Array is successfully created");
 
 
-        } catch (Exception e) {
+        } catch (NegativeArraySizeException e) {
+            System.out.println("array size cannot be negative");
 
         }
     }
+
 }
