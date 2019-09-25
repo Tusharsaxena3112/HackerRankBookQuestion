0f0479f2eaf604530955d8ddb1bd2c4f5e185ef3
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-25 23:30:58 +0530

    adding object of java.util.Random

--- src/exceptionHandling/ExceptionHandling.java
+++ src/exceptionHandling/ExceptionHandling.java
@@ -1,5 +1,15 @@
 package exceptionHandling;
 
-public class ExceptionHandling {
+import java.util.Random;
 
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        try {
+            Random input = new Random();
+
+
+        } catch (Exception e) {
+
+        }
+    }
 }
