0bd48514c9264fede1d4a0da39eb1118b0cd32a9
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-01 23:17:16 +0530

    adding object for Question6.java

--- src/stringHandling/Main.java
+++ src/stringHandling/Main.java
@@ -9,6 +9,7 @@ public class Main {
         //Question2 obj1=new Question2(input.nextLine(),input.nextInt());
         //Question3 obj3 = new Question3(input.nextLine());
         //Question5 obj5 = new Question5(input.nextLine(), input.nextLine(), input.nextLine());
+        //Question6 obj6=new Question6(input.nextLine());
 
 
     }
