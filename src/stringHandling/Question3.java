6d35f2806b352dcae881d0abe638b96b8fbedbc5
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-01 22:40:56 +0530

    adding functionality to Question3.java

--- src/stringHandling/Question3.java
+++ src/stringHandling/Question3.java
@@ -1,5 +1,20 @@
 package stringHandling;
 
 public class Question3 {
+    private String inputString;
 
+    public Question3(String inputString) {
+        this.inputString = inputString;
+        printString(inputString);
+    }
+
+    public void printString(String inputString) {
+        inputString = inputString.toLowerCase();
+        for (int i = 0; i <= inputString.length() - 1; i++) {
+            if (inputString.charAt(i) == 'a' || inputString.charAt(i) == 'e' || inputString.charAt(i) == 'i' || inputString.charAt(i) == 'o' || inputString.charAt(i) == 'u') {
+                inputString = inputString.replace(inputString.charAt(i), '*');
+            }
+        }
+        System.out.println(inputString);
+    }
 }
