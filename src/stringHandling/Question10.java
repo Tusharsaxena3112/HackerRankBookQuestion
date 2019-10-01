7e20000a1fb7fbbd06f784f8186da9d2973e4af4
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-02 00:33:55 +0530

    adding functionality to Question10.java

--- src/stringHandling/Question10.java
+++ src/stringHandling/Question10.java
@@ -1,5 +1,14 @@
 package stringHandling;
 
 public class Question10 {
+    private String inputString;
+    private char a;
+
+    public Question10(String inputString, char a) {
+        this.inputString = inputString;
+        this.a = a;
+        System.out.println(inputString.indexOf(a));
+    }
+
 
 }
