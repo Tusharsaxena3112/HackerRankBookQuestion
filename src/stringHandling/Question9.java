bf81ef8b021fefe4ddc2c6df338ad4d1dcfa5637
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-02 00:29:26 +0530

    adding functionality to Question9.java

--- src/stringHandling/Question9.java
+++ src/stringHandling/Question9.java
@@ -1,5 +1,30 @@
 package stringHandling;
 
 public class Question9 {
+    private String inputString;
 
+    public Question9(String inputString) {
+        this.inputString = inputString;
+        System.out.println(printUpperWeight(inputString) - printLowerWeight(inputString));
+    }
+
+    public int printUpperWeight(String inputString) {
+        int weight = 0;
+        for (int i = 0; i <= inputString.length() - 1; i++) {
+            if ((int) inputString.charAt(i) >= 65 && (int) inputString.charAt(i) <= 91) {
+                weight += (int) inputString.charAt(i);
+            }
+        }
+        return weight;
+    }
+
+    public int printLowerWeight(String inputString) {
+        int weight = 0;
+        for (int i = 0; i <= inputString.length() - 1; i++) {
+            if ((int) inputString.charAt(i) >= 97 && (int) inputString.charAt(i) <= 122) {
+                weight += (int) inputString.charAt(i);
+            }
+        }
+        return weight;
+    }
 }
