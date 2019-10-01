9f923de50905b15b35bbf40852e6a6f42ca7056a
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-01 23:16:15 +0530

    final commit Question6.java

--- src/stringHandling/Question6.java
+++ src/stringHandling/Question6.java
@@ -1,6 +1,10 @@
 package stringHandling;
 
 public class Question6 {
+    private String inputString;
 
-
+    public Question6(String inputString) {
+        this.inputString = inputString;
+        System.out.println(inputString.toUpperCase());
+    }
 }
