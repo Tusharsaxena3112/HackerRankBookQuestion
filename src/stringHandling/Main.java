a9c9c1501bf1dee3484691b5c2798625728cc343
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-02 00:35:11 +0530

    adding object of Question10.java

--- src/stringHandling/Main.java
+++ src/stringHandling/Main.java
@@ -13,6 +13,7 @@ public class Main {
         // Question7 obj7 = new Question7(input.nextLine());
         //Question8 obj8=new Question8(input.nextLine());
         //Question9 obj9= new Question9(input.nextLine());
+        Question10 obj10 = new Question10(input.nextLine(), input.next().charAt(0));
 
 
 
