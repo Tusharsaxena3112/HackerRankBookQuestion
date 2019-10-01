a3334885b5d04a40fbb01c94421283d02c8f3f8e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-01 23:47:16 +0530

    adding functionlity to Question8.java

--- src/stringHandling/Question8.java
+++ src/stringHandling/Question8.java
@@ -1,4 +1,21 @@
 package stringHandling;
 
 public class Question8 {
+    private String inputString;
+
+    public Question8(String inputString) {
+        this.inputString = inputString;
+        printUpperWeight(inputString);
+    }
+
+    public void printUpperWeight(String inputString) {
+        int weight = 0;
+        for (int i = 0; i <= inputString.length() - 1; i++) {
+            if ((int) inputString.charAt(i) >= 65 && (int) inputString.charAt(i) <= 91) {
+                weight += (int) inputString.charAt(i);
+            }
+        }
+
+        System.out.println(weight);
+    }
 }
