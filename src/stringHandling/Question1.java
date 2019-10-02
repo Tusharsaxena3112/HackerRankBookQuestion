6766ab0de6155f602dffc8db3c81ed459fad135e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-02 12:27:46 +0530

    adding comments to Question1.java

--- src/stringHandling/Question1.java
+++ src/stringHandling/Question1.java
@@ -1,5 +1,14 @@
 package stringHandling;
 
+//WAP to print any given inputString in following format.
+//
+//Output
+//
+//index:  0    1   2   3   4
+//chars:  H    e   l   l   o
+//
+//Every character is seprated by tab, use "\t" for inserting tab.
+
 public class Question1 {
     private String givenString;
 
@@ -9,14 +18,14 @@ public class Question1 {
     }
 
     public void printString(String givenString) {
-        System.out.printf("index: ");
+        System.out.printf("index:\t");
         for (int i = 0; i <= givenString.length() - 1; i++) {
-            System.out.printf("%d ", i);
+            System.out.printf("%d\t", i);
         }
         System.out.println();
-        System.out.printf("chars: ");
+        System.out.printf("chars:\t");
         for (int i = 0; i <= givenString.length() - 1; i++) {
-            System.out.printf("%c ", givenString.charAt(i));
+            System.out.printf("%c\t", givenString.charAt(i));
         }
     }
 }
