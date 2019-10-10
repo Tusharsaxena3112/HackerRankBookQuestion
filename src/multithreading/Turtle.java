58f2a2a5b00e3a786eb02298d40c4696793d4c46
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-10 23:18:44 +0530

    overriding run() method in Turtle.java

--- src/multithreading/Turtle.java
+++ src/multithreading/Turtle.java
@@ -5,4 +5,19 @@ public class Turtle extends Thread {
         super(name);
     }
 
+    @Override
+    public void run() {
+        System.out.println(getName() + " has started the race");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(getName() + " has reached mountain.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
 }
