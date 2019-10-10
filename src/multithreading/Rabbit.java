2c0c8be4ff032a4ab823ca72189a36e7a8bcc370
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-10 23:07:27 +0530

    completing Rabbit.java

--- src/multithreading/Rabbit.java
+++ src/multithreading/Rabbit.java
@@ -1,9 +1,24 @@
 package multithreading;
 
 public class Rabbit extends Thread {
+    public Rabbit(String name) {
+        super(name);// Name of the Thread.
+    }
 
     @Override
     public void run() {
-        System.out.println("Rabbit has started its Race");
+        System.out.println(getName() + " has started its Race");
+        try {                                      // Try catch as this can generate exception.
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(getName() + " Reached Mountain");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(getName() + " Has finished Race");
     }
 }
