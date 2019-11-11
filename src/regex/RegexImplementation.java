2f33b46536c68ea998199f3edbfbba1ed40c56aa
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-11 22:41:59 +0530

    adding code to RegexImplementation.java

--- src/regex/RegexImplementation.java
+++ src/regex/RegexImplementation.java
@@ -1,7 +1,22 @@
 package regex;
 
-public class RegexImplementation {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class RegexImplementation {
+    //Email format:
+    //text@text.com
+    //text-text@text.com
+    //text-text-text.text@text.com
+    //text@text-text.com
+    //Test@text.com
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the text");
+        String string = scanner.nextLine();
+        Pattern pattern = Pattern.compile("");
+        Matcher matcher = pattern.matcher(string);
+        System.out.println(matcher.matches());
     }
 }
