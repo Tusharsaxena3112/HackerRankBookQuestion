1c057d082379761f89b15d1ab4610e80069615e5
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-11 23:39:49 +0530

    complete

--- src/regex/RegexOccurence.java
+++ src/regex/RegexOccurence.java
@@ -7,14 +7,14 @@ public class RegexOccurence {
     public static void main(String[] args) {
         //Scanner scanner = new Scanner(System.in);
         String text = "This is the string text and is very helpful";
-        Pattern pattern = Pattern.compile("(is)+");
+        Pattern pattern = Pattern.compile(" (is)+");
         Matcher matcher = pattern.matcher(text);
         System.out.println(matcher.matches());
         matcher.reset();
         int count = 0;
         while (matcher.find()) {
             count++;
-            System.out.println("Occurrence: " + count + "from" + matcher.start() + " to " + matcher.end());
+            System.out.println("Occurrence: " + count + " from " + matcher.start() + " to " + matcher.end());
         }
     }
 }
