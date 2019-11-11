56515c53991541ae81e352f0e2621f4c47f69b75
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-11 23:28:39 +0530

    adding Pattern and Matcher instances in RegexOccurence.java

--- src/regex/RegexOccurence.java
+++ src/regex/RegexOccurence.java
@@ -1,7 +1,15 @@
 package regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexOccurence {
     public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        String text = "This is the string text";
+        Pattern pattern = Pattern.compile("");
+        Matcher matcher = pattern.matcher(text);
+        System.out.println(matcher.matches());
 
     }
 
