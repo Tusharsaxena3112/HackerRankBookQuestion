17937081cfecabdcd6a4f9d1d850005ff625f920
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-15 16:47:02 +0530

    adding CreditCard.java

---
+++ src/regex/CreditCard.java
@@ -0,0 +1,11 @@
+package regex;
+
+import java.util.Scanner;
+
+public class CreditCard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Credit card no.:");
+        int number =
+    }
+}
