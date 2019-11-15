0b94e9ff6dc37309b0fe6a3756b70efd5cecaa8f
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-15 16:56:45 +0530

    complete

--- src/regex/CreditCard.java
+++ src/regex/CreditCard.java
@@ -1,11 +1,16 @@
 package regex;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CreditCard {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Credit card no.:");
-        int number =
+        String number = scanner.nextLine();
+        Pattern pattern = Pattern.compile("^([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})$|^\\d{16}$");
+        Matcher matcher = pattern.matcher(number);
+        System.out.println(matcher.matches());
     }
 }
